6fe269c994a48643e58fcf11719b2ae8390a4d31
gabriel_rc__ <ganriel201@gmail.com>
2023-03-07 16:22:09 -0300

    entidade convidado

---
+++ src/main/java/com/eventoapp/eventoapp/models/Convidado.java
@@ -0,0 +1,62 @@
+package com.eventoapp.eventoapp.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Convidado implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne
+    private Evento evento;
+
+    @Column(nullable = false, unique = true)
+    private String rg;
+
+    @Column(nullable = false)
+    private String nomeConvidado;
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getNomeConvidado() {
+        return nomeConvidado;
+    }
+
+    public void setNomeConvidado(String nomeConvidado) {
+        this.nomeConvidado = nomeConvidado;
+    }
+
+}
